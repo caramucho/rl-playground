ef155488e51011b24014bf4a44cb55291ed87ae3
Caramucho <zl.iafag@gmail.com>
2019-11-03 18:43:26 +0900

    add fittinh

---
+++ fitting.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import math
+
+
+samples = 100
+x_train = [2 + i/samples * 5.5 for i in range(samples)]
+y_train = [100*math.sin(x) for x in x_train]
+
+plt.plot(x_train, y_train)
+
+
+class regression(nn.Module):
+    def __init__(self):
+        super(regression, self).__init__()
+        self.my_d = nn.Parameter(torch.randn(1))
+        self.my_c = nn.Parameter(torch.randn(1))
+        self.my_b = nn.Parameter(torch.randn(1))
+        self.my_a = nn.Parameter(torch.randn(1))
+        # self.my_c = nn.Parameter(torch.Tensor([10]))
+        # self.my_b = nn.Parameter(torch.Tensor([-8]))
+        # self.my_a = nn.Parameter(torch.Tensor([1]))
+
+        # self.not_param = Variable(torch.randn(1), requires_grad=True)
+
+    def forward(self, x):
+        return +self.my_d*x*x*x + self.my_a * x * x + self.my_b * x + self.my_c
+
+
+learning_rate = 1
+model = regression()
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+num_epochs = 10000
+for epoch in range(num_epochs):
+    inputs = torch.Tensor(x_train)
+    targets = torch.Tensor(y_train)
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+    print(loss)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+samples = 100
+x_test = [1.5 + i/samples * 6.5 for i in range(samples)]
+y_test = [model.my_d*x*x*x + model.my_a * x *
+          x + model.my_b*x+model.my_c for x in x_test]
+
+plt.plot(x_test, y_test)
+plt.plot(x_train, y_train)
